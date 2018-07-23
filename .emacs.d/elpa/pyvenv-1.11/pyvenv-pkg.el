3a53f06834b1c35fb0fd79f1566920b7edf689bd
Luís Zilhão <lzilhao94@gmail.com>
2018-07-23 16:03:14 +0100

    Fixed symbolic links again

---
+++ .emacs.d/elpa/pyvenv-1.11/pyvenv-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pyvenv" "1.11" "Python virtual environment interface" 'nil :commit "f925bcb46ea64b699f7cd06933c48e0d5db88b73" :url "http://github.com/jorgenschaefer/pyvenv" :keywords '("python" "virtualenv" "tools"))
